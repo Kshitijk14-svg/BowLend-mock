7242520b3bcab7f09589c5a27df6ffbced911799
Kshitijk14-svg <kshitijmay14@gmail.com>
2026-01-14 22:09:49 +0530

    The backend Spring initializer file is added

---
+++ BowLend/src/main/java/com/example/BowLend/BowLendApplication.java
@@ -0,0 +1,13 @@
+package com.example.BowLend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowLendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowLendApplication.class, args);
+	}
+
+}
---
+++ BowLend/src/test/java/com/example/BowLend/BowLendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BowLend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowLendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
